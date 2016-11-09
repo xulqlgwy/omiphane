b91e11a3261d773c7a850f695e418d636110d4bb
xulq <xulqlgwy@126.com>
2016-11-10 00:39:10 +0800

    aaadfff

--- src/main/resources/sql/omiphane.sql
+++ src/main/resources/sql/omiphane.sql
@@ -1,116 +1,60 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 192.168.85.128-vm-mysql
-Source Server Version : 50173
-Source Host           : 192.168.85.128:3306
+Source Server         : localhost
+Source Server Version : 50634
+Source Host           : localhost:3306
 Source Database       : omiphane
 
 Target Server Type    : MYSQL
-Target Server Version : 50173
+Target Server Version : 50634
 File Encoding         : 65001
 
-Date: 2016-05-27 00:30:11
+Date: 2016-11-10 00:38:41
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for device
+-- Table structure for device_base
 -- ----------------------------
-DROP TABLE IF EXISTS `device`;
-CREATE TABLE `device` (
+DROP TABLE IF EXISTS `device_base`;
+CREATE TABLE `device_base` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `device_id` varchar(64) DEFAULT NULL,
-  `device_type` int(10) DEFAULT NULL,
-  `device_sn` varchar(128) DEFAULT NULL,
   `device_name` varchar(255) DEFAULT NULL,
-  `parent_node_id` int(11) DEFAULT NULL COMMENT '关联的节点id',
+  `device_typ` tinyint(4) DEFAULT NULL,
+  `nick_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of device
+-- Records of device_base
 -- ----------------------------
-INSERT INTO `device` VALUES ('1', '1001', '1', 'ADD_QSS111', '车辆终端', null);
+INSERT INTO `device_base` VALUES ('1', '100001', 'CLD-X-1000001', '1', 'CLD-X-1000001');
+INSERT INTO `device_base` VALUES ('2', '100002', 'CLD-X-1000002', '1', 'CLD-X-1000002');
 
 -- ----------------------------
--- Table structure for device_data
+-- Table structure for device_real
 -- ----------------------------
-DROP TABLE IF EXISTS `device_data`;
-CREATE TABLE `device_data` (
+DROP TABLE IF EXISTS `device_real`;
+CREATE TABLE `device_real` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `device_id` int(11) DEFAULT NULL,
-  `prop_enum` int(11) DEFAULT NULL COMMENT '属性名定义在 enum_define 表',
-  `prop_data` varchar(511) DEFAULT NULL,
+  `device_id` varchar(64) DEFAULT NULL,
+  `speed` int(11) DEFAULT NULL,
+  `weight` double DEFAULT NULL,
+  `latitude` double DEFAULT NULL COMMENT '维度',
+  `longtitude` double DEFAULT NULL,
+  `user_data` varchar(255) DEFAULT NULL,
+  `parameter` varchar(1024) DEFAULT NULL COMMENT 'json 参数',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of device_data
+-- Records of device_real
 -- ----------------------------
-INSERT INTO `device_data` VALUES ('1', '1001', '1', 'jdjdj');
-
--- ----------------------------
--- Table structure for device_node_rel
--- ----------------------------
-DROP TABLE IF EXISTS `device_node_rel`;
-CREATE TABLE `device_node_rel` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `device_id` varchar(255) DEFAULT NULL,
-  `parent_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of device_node_rel
--- ----------------------------
-INSERT INTO `device_node_rel` VALUES ('1', '1001', '101');
-
--- ----------------------------
--- Table structure for enum_define
--- ----------------------------
-DROP TABLE IF EXISTS `enum_define`;
-CREATE TABLE `enum_define` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `enum_name` varchar(255) DEFAULT NULL,
-  `enum_value` varchar(11) DEFAULT NULL,
-  `memo` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of enum_define
--- ----------------------------
-INSERT INTO `enum_define` VALUES ('1', 'NODE_TYPE', 'DEV', '终端');
-INSERT INTO `enum_define` VALUES ('2', 'NODE_TYPE', 'COM', '公司');
-INSERT INTO `enum_define` VALUES ('3', 'NODE_TYPE', 'NODE', '节点');
-
--- ----------------------------
--- Table structure for node
--- ----------------------------
-DROP TABLE IF EXISTS `node`;
-CREATE TABLE `node` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `parent_id` int(11) DEFAULT NULL,
-  `text` varchar(128) DEFAULT NULL,
-  `leaf` varchar(10) DEFAULT NULL COMMENT '是否是子节点',
-  `cls` int(11) DEFAULT NULL,
-  `icon_cls` varchar(64) DEFAULT NULL COMMENT '图片路径',
-  `checked` varchar(10) DEFAULT NULL,
-  `page` varchar(64) DEFAULT NULL,
-  `node_type` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of node
--- ----------------------------
-INSERT INTO `node` VALUES ('1', '0', 'aaaa', '0', '1', '', 'false', 'device', null);
-INSERT INTO `node` VALUES ('100', '1', 'bbbb', '221', '12', null, 'true', 'device', null);
-INSERT INTO `node` VALUES ('101', '100', 'cccc', '1', '2', null, 'false', 'company', null);
-INSERT INTO `node` VALUES ('102', '0', 'ddd', '0', '1', '', 'false', null, 'node');
-INSERT INTO `node` VALUES ('103', '102', 'deeee', '1', '11', null, 'false', null, 'node');
+INSERT INTO `device_real` VALUES ('1', '100001', '120', '12.3', '30.23652', '120.25656', null, null);
+INSERT INTO `device_real` VALUES ('2', '100002', '123', '12.6', '30.99999', '120.4444', null, null);
 
 -- ----------------------------
 -- Table structure for role_permission
